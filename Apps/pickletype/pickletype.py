cc27b17c938b8c94799d28de2f63a3c87a82f784
Jeff-web-sketch <coderdude123@proton.me>
2025-10-19 08:17:24 -0400

    made it just freezedriedgoodies

--- Apps/pickletype/pickletype.py
+++
@@ -1,5 +0,0 @@
-import webview
-
-# Load the local HTML file
-webview.create_window("Pickle Type", "file:////Users/bobrobertson/Documents/GitHub/Jeff-web-sketch.github.io/index.html", width=1000, height=700)
-webview.start()
